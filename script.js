51c1beb2f756c78add74d3314ec974df970ce9c4
TechJeeper <caffeinegamer@gmail.com>
2026-05-15 14:35:22 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+(function () {
+  const coin = document.getElementById("flip-coin");
+  const inner = coin?.querySelector(".coin-inner");
+  if (!coin || !inner) return;
+
+  let rotation = 0;
+  let flipping = false;
+
+  coin.addEventListener("click", () => {
+    if (flipping) return;
+    flipping = true;
+
+    coin.classList.remove("is-paused");
+    coin.classList.add("is-flipping");
+
+    const extra = 1440 + Math.floor(Math.random() * 360);
+    const start = rotation % 360;
+    rotation += extra;
+
+    inner.style.setProperty("--spin-start", start + "deg");
+    inner.style.setProperty("--spin-end", rotation + "deg");
+
+    const onEnd = () => {
+      inner.removeEventListener("animationend", onEnd);
+      coin.classList.remove("is-flipping");
+      inner.style.animation = "none";
+      inner.style.transform = "rotateY(" + (rotation % 360) + "deg)";
+      void inner.offsetWidth;
+      inner.style.animation = "";
+      flipping = false;
+
+      setTimeout(() => {
+        if (!flipping) coin.classList.remove("is-paused");
+      }, 2000);
+    };
+
+    inner.addEventListener("animationend", onEnd);
+    coin.classList.add("is-paused");
+  });
+
+  coin.addEventListener("mouseenter", () => {
+    if (!flipping) coin.classList.add("is-paused");
+  });
+
+  coin.addEventListener("mouseleave", () => {
+    if (!flipping) coin.classList.remove("is-paused");
+  });
+})();
